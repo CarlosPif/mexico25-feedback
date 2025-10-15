5ca7f9ca29bc3d6c30142fca52a264a5c4926ec8
Carlos Martinez <cmartinez@sancuscapital.es>
2025-10-15 16:49:30 +0200

    s

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+Home = st.Page(
+    "my_app/Home.py", title="Risk / Reward", icon=":material/dashboard:", default=True
+)
+
+General-overview = st.Page(
+    "my_app/pages/General-overview.py", title="Details", icon=":material/details:", default=True
+)
+
+pg = st.navigation(
+    {
+        "Menorca 2025": [],
+        "Mexico 2025": [Home, General-overview],
+    }
+)
+
+pg.run()
